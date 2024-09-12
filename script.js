7f2127c226bd31336629cb200fa7fb05e77c4c5b
Fabricio Basilio <fabriciobasilioo@gmail.com>
2024-09-12 00:11:16 -0300

    Configurar segundos e minutos

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const botaoIniciar = document.getElementById("botaoIniciar");
 const botaoPausar = document.getElementById("botaoPausar");
 const botaoMarcar = document.getElementById("botaoMarcar");
 const spanMilissegundos = document.getElementById("milissegundos");
-const spanSegundos = document.getElementById("minutos");
-const spanMinutos = document.getElementById("segundos");
+const spanSegundos = document.getElementById("segundos");
+const spanMinutos = document.getElementById("minutos");
 
 let milissegundos, minutos, segundos;
 
@@ -23,8 +23,33 @@ function incrementar() {
 
         milissegundos++;
 
-        if (milissegundos >= 10)spanMilissegundos.innerText = milissegundos;
+        if (milissegundos === 100) {
+            milissegundos = 0;
+            segundos++;
+
+            spanSegundos.innerText = `0${segundos}`
+        }
+
+        if (milissegundos >= 10) spanMilissegundos.innerText = milissegundos;
         else spanMilissegundos.innerText = `0${milissegundos}`
 
-    }, 100);
+        if (segundos === 60) {
+            segundos = 0;
+            minutos++;
+
+            spanMinutos.innerText = `0${minutos}`;
+        }
+
+        if (minutos >= 10)
+            spanMinutos.innerText = minutos;
+        else spanMinutos.innerText = `0${minutos}`
+
+
+
+        if (segundos >= 10) spanSegundos.innerText = segundos;
+        else spanSegundos.innerText = `0${segundos}`
+
+
+
+    }, 0.1);
 }
